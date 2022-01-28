c7453195dbf700262bacd812c923f24350f256d0
Suchir Kavi <suchirkavi@gmail.com>
2022-01-28 17:32:08 -0600

    iterate over dict instead of over include again

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::collections::HashSet;
 use rayon::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     let words = include_str!("../words_alpha.txt");
 
-    let dict = words
-        .par_lines()
-        .collect::<HashSet<_>>();
-    
-    let mut results = words
-        .par_lines()
+    let dict = words.par_lines().collect::<HashSet<_>>();
+
+    let mut results = dict
+        .par_iter()
         .filter(|word| word.len() & 1 == 0)
         .map(deinterleave)
         .filter(|(wr, od, _)| dict.contains(wr.as_str()) && dict.contains(od.as_str()))
@@ -22,8 +20,10 @@ fn main() {
     }
 }
 
-fn deinterleave(word: &str) -> (String, String, &str) {
-    (word.chars().step_by(2).collect(),
-     word.chars().skip(1).step_by(2).collect(),
-     word)
+fn deinterleave<'a>(word: &'a &str) -> (String, String, &'a str) {
+    (
+        word.chars().step_by(2).collect(),
+        word.chars().skip(1).step_by(2).collect(),
+        word,
+    )
 }
